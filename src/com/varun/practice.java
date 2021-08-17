a4423cb7b1d6408c189498f137891da36c428ec1
vsinghal <varun.singhal@amadeus.com>
2021-08-17 18:15:36 +0530

    added adapter

--- src/com/varun/practice.java
+++ src/com/varun/practice.java
@@ -2,75 +2,35 @@ package com.varun;
 
 class Driver {
     public static void main(String[] args) {
-        String str = new NutritionFacts.Builder(2, 4)
-                .withCalcium(6)
-                .withMagnesium(7)
-                .withSodium(10)
-                .build()
-                .toString();
-        System.out.println(str);
+
+        DistanceServiceClient service = new KiloToMetresAdapter(new DistanceService());
+        System.out.println(service.getDistanceInMetres());
+
+    }
+
+    private interface DistanceServiceClient {
+        int getDistanceInMetres();
+    }
+
+    private static class DistanceService {
+        public int getDistanceInKiloMetres() {
+            return 10;
+        }
+    }
+
+    private static class KiloToMetresAdapter implements DistanceServiceClient {
+
+        private final DistanceService distanceService;
+
+        KiloToMetresAdapter(DistanceService distanceService) {
+            this.distanceService = distanceService;
+        }
+
+        @Override
+        public int getDistanceInMetres() {
+            return distanceService.getDistanceInKiloMetres()*1000;
+        }
     }
 }
 
-class NutritionFacts {
-    //optional
-    int sodium;
-    int magnesium;
-    int calcium;
-    //mandatory
-    int servingsSize;
-    int numberOfServings;
 
-    private NutritionFacts(Builder builder) {
-        this.sodium = builder.sodium;
-        this.magnesium = builder.magnesium;
-        this.calcium = builder.calcium;
-        this.servingsSize = builder.servingsSize;
-        this.numberOfServings = builder.numberOfServings;
-    }
-
-    public static class Builder {
-        //optional
-        int sodium;
-        int magnesium;
-        int calcium;
-        //mandatory
-        int servingsSize;
-        int numberOfServings;
-
-        Builder(int servingsSize, int numberOfServings) {
-            this.servingsSize = servingsSize;
-            this.numberOfServings = numberOfServings;
-        }
-
-        public Builder withSodium(int amount) {
-            this.sodium = amount;
-            return this;
-        }
-
-        public Builder withMagnesium(int amount) {
-            this.magnesium = amount;
-            return this;
-        }
-
-        public Builder withCalcium(int amount) {
-            this.calcium = amount;
-            return this;
-        }
-
-        public NutritionFacts build() {
-            return new NutritionFacts(this);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "NutritionFacts{" +
-                "sodium=" + sodium +
-                ", magnesium=" + magnesium +
-                ", calcium=" + calcium +
-                ", servingsSize=" + servingsSize +
-                ", numberOfServings=" + numberOfServings +
-                '}';
-    }
-}
--- src/com/varun/structural/Adapter.java
+++ src/com/varun/structural/Adapter.java
@@ -2,7 +2,7 @@ package com.varun.structural;
 
 /**
  * https://www.vogella.com/tutorials/DesignPatternAdapter/article.html
- *
+ * https://refactoring.guru/design-patterns/adapter
  *  The adapter pattern describes how to convert an object into another object which a clients expects.
  *  This pattern mainly adapts one object to another one.
  *  Adapters allow objects to work together that couldn’t otherwise because of incompatible interfaces
